f71f312a68cc34ca45b73b5644d9d4162f22bc61
DJosueMM <djosuemm@gmail.com>
2024-07-25 22:21:00 -0600

    archivos para el tutorial

--- ejemplos/BlinkyLed/src/design/blinky.v
+++ ejemplos/BlinkyLed/src/design/blinky.v
@@ -1,10 +1,10 @@
-module top
+module blinky_led
 (
     input clk,
     output [5:0] led
 );
 
-localparam WAIT_TIME = 13500000
+localparam WAIT_TIME = 13500000;
 reg [5:0] ledCounter = 0;
 reg [23:0] clockCounter = 0;
 
